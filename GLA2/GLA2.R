7d5c78e4b03af0052e9c761694b1581618f73c51
starship9 <nishanksaini10@gmail.com>
2017-03-31 01:17:01 +0530

    Retrieving required parameters

--- GLA2/GLA2.R
+++ GLA2/GLA2.R
@@ -1,10 +1,12 @@
 library(readr)
+library(magrittr)
 annual_all_2016 <- read_csv("~/Desktop/SNU/RStuff/GLA2/annual_all_2016.csv")
 
 head(annual_all_2016)
 names(annual_all_2016)
 library(dplyr)
-reqData<-select(annual_all_2016, `Parameter Name`, `Sample Duration`, `State Name`, `County Name`, `City Name`)
+#getting required data parameters, including only those that have been completed
+reqData<-select(annual_all_2016, `Parameter Name`, `Observation Count`,`Completeness Indicator`,`Sample Duration` ,`State Name`, `County Name`, `City Name`)%>%filter(`Completeness Indicator`!='N')
 head(reqData)
-
+View(reqData)
 
