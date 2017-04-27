c306a032b72a64eda1c7a8587dfee1563321e7da
starship9 <nishanksaini10@gmail.com>
2017-04-27 15:38:46 +0530

    Removing stopwords

--- NonGradedAssignmentR/question3.R
+++ NonGradedAssignmentR/question3.R
@@ -2,12 +2,21 @@ setwd("/home/starship9/Desktop/SNU/Data Mining/CSD342_DataMining/NonGradedAssign
 
 #creating a function for visualising the frequent itemsets of a file x, and the min supp threshold freq
 genWordGraph<-function(x,freq){
-
+library(tm)
 #taking the raw text  
 raw.text<-scan(x,what="char",sep="")
 #head(raw.text)
 #converting to lower case
 raw.text<-tolower(raw.text)
+#whatsappDF$V6<-gsub('\\Saini+', '', whatsappDF$V6)
+raw.text<-gsub(' a ','',raw.text)
+raw.text<-gsub(' an ','',raw.text)
+raw.text<-gsub(' of ','',raw.text)
+raw.text<-gsub(' is ','',raw.text)
+raw.text<-gsub(' the ','',raw.text)
+raw.text<-gsub(' and ','',raw.text)
+raw.text<-gsub(' is ','',raw.text)
+#raw.text<-tm_map(x,removeWords,stopwords("english"))
 #splitting wrt whitespace
 wordList.txt<-strsplit(raw.text,"\\W+",perl=TRUE)
 #head(wordList.txt)
@@ -18,6 +27,7 @@ wordVector.txt<-unlist(wordList.txt)
 
 #using table() to get the frequency count
 text.freq.list<-table(wordVector.txt)
+#text.freq.list
 library(dplyr)
 
 #converting into a DF
@@ -33,6 +43,10 @@ sample_n(textDF,1)
 
 #getting valid itemsets
 textMoreThan10Freq<-filter(textDF,textDF$Frequency>freq)
+#textMoreThan10Freq<-gsub('\\a+', '', textMoreThan10Freq)
+#textMoreThan10Freq<-gsub('\\an+', '', textMoreThan10Freq)
+#textMoreThan10Freq<-gsub('\\the+', '', textMoreThan10Freq)
+#textMoreThan10Freq<-gsub('\\was+', '', textMoreThan10Freq)
 head(textMoreThan10Freq)
 #text.sortedFreq.list <- sort(text.freq.list)
 #head(text.sortedFreq.list)
@@ -42,7 +56,7 @@ head(textMoreThan10Freq)
 library(ggplot2)
 
 #graph
-g<-ggplot(textMoreThan10Freq,aes(x=textMoreThan10Freq$Word, y = textMoreThan10Freq$Frequency, fill = textMoreThan10Freq$Word)) + geom_bar(stat="identity")
+g<-ggplot(textMoreThan10Freq,aes(x=textMoreThan10Freq$Word, y = textMoreThan10Freq$Frequency, fill = textMoreThan10Freq$Word)) + geom_bar(stat="identity") + labs(x = "Word", y = "Frequency", title = "Non Graded Assignment")
 #getwd()
 return (g)
 }
@@ -52,5 +66,6 @@ genWordGraph("largeTxt.txt",2000)
 #graph for the smaller text file
 genWordGraph("ngla1.txt",10)
 
-
+#library(wordcloud)
+#wordcloud(textMoreThan10Freq$Word,  random.order = FALSE, max.words = 100, col = brewer.pal(10,"Set3"))
 
