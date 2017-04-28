bc640486d5cb14ba3202ef001c6aa0e3f153099a
starship9 <nishanksaini10@gmail.com>
2017-04-28 13:01:52 +0530

    Eh, whatever works

--- NonGradedAssignmentR/question3.R
+++ NonGradedAssignmentR/question3.R
@@ -1,3 +1,9 @@
+#CSD342: Data Mining and Data warehousing
+#Graded lab assignment 4
+#Group 2
+#Manjul Singh Sachan (1410110228)
+#Mohak Garg (1410110247)
+#Nishank Saini (1410110266)
 setwd("/home/starship9/Desktop/SNU/Data Mining/CSD342_DataMining/NonGradedAssignmentR")
 
 #creating a function for visualising the frequent itemsets of a file x, and the min supp threshold freq
@@ -10,15 +16,15 @@ genWordGraph<-function(x,freq){
   raw.text<-tolower(raw.text)
   #raw.text <- tm_map(raw.text, removeWords, stopwords("english"))
   #whatsappDF$V6<-gsub('\\Saini+', '', whatsappDF$V6)
-  raw.text<-gsub('+that+','',raw.text)
-  raw.text<-gsub('+a+','',raw.text)
-  raw.text<-gsub('+an+','',raw.text)
-  raw.text<-gsub('+of+','',raw.text)
-  raw.text<-gsub('+is+','',raw.text)
-  raw.text<-gsub('+the+','',raw.text)
-  raw.text<-gsub('+and+','',raw.text)
-  raw.text<-gsub('+in+','',raw.text)
-  raw.text<-gsub('+it+','',raw.text)
+  raw.text<-gsub(' that ','',raw.text)
+  raw.text<-gsub(' a ','',raw.text)
+  raw.text<-gsub(' an ','',raw.text)
+  raw.text<-gsub(' of ','',raw.text)
+  raw.text<-gsub(' is ','',raw.text)
+  raw.text<-gsub(' the ','',raw.text)
+  raw.text<-gsub(' and ','',raw.text)
+  raw.text<-gsub(' in ','',raw.text)
+  raw.text<-gsub(' it ','',raw.text)
   #raw.text<-tm_map(x,removeWords,stopwords("english"))
   #splitting wrt whitespace
   wordList.txt<-strsplit(raw.text,"\\W+",perl=TRUE)
@@ -63,29 +69,33 @@ genWordGraph<-function(x,freq){
   #getwd()
 return (g)
   
-  library(arules)
+#  library(arules)
   
   #textMoreThan10Freq$Word<-discretize(as.String(textMoreThan10Freq$Word))
   #textMoreThan10Freq$Frequency<-discretize(as.numeric(textMoreThan10Freq$Frequency))
-  data <- data.frame(sapply(textMoreThan10Freq,as.factor))
+ 
+ #attempting apriori
+  #data <- data.frame(sapply(textMoreThan10Freq,as.factor))
   
-  rules<-apriori(data)
-  arules::inspect(rules)
-  rules.sorted<-sort(rules,by="lift")
-  arules::inspect(rules.sorted)
+  #rules<-apriori(data)
+  #arules::inspect(rules)
+  #rules.sorted<-sort(rules,by="lift")
+  #arules::inspect(rules.sorted)
   
-  library(arulesViz)
+  #library(arulesViz)
   
-  plot(rules, method="graph", control=list(type="items"))
-  library(wordcloud)
-  wordcloud(textMoreThan10Freq$Word, random.order = FALSE, max.words = 30, col = brewer.pal(9,"Blues"))
+  #plot(rules, method="graph", control=list(type="items"))
+  
+  #generating a wordcloud
+  #library(wordcloud)
+  #wordcloud(textMoreThan10Freq$Word, random.order = FALSE, max.words = 300, col = brewer.pal(9,"Blues"),random.color = TRUE)
 
   #plot(rules)
   #plot(rules, method="paracoord", control=list(reorder=TRUE))
 }
 #graph for the larger text file
 
-genWordGraph("largeTxt.txt",1000)
+genWordGraph("largeTxt.txt",500)
 
 #graph for the smaller text file
 genWordGraph("ngla1.txt",10)
@@ -93,3 +103,58 @@ genWordGraph("ngla1.txt",10)
 #library(wordcloud)
 #wordcloud(textMoreThan10Freq$Word,  random.order = FALSE, max.words = 100, col = brewer.pal(10,"Set3"))
 
+freq<-50
+raw.text<-scan("largeTxt.txt",what="char",sep="")
+#head(raw.text)
+#converting to lower case
+raw.text<-tolower(raw.text)
+#raw.text <- tm_map(raw.text, removeWords, stopwords("english"))
+#whatsappDF$V6<-gsub('\\Saini+', '', whatsappDF$V6)
+raw.text<-gsub(' that ','',raw.text)
+raw.text<-gsub(' a ','',raw.text)
+raw.text<-gsub(' an ','',raw.text)
+raw.text<-gsub(' of ','',raw.text)
+raw.text<-gsub(' is ','',raw.text)
+raw.text<-gsub(' the ','',raw.text)
+raw.text<-gsub(' and ','',raw.text)
+raw.text<-gsub(' in ','',raw.text)
+raw.text<-gsub(' it ','',raw.text)
+#raw.text<-tm_map(x,removeWords,stopwords("english"))
+#splitting wrt whitespace
+wordList.txt<-strsplit(raw.text,"\\W+",perl=TRUE)
+#head(wordList.txt)
+
+#storing a list as a vector
+wordVector.txt<-unlist(wordList.txt)
+#head(wordVector.txt)
+
+#using table() to get the frequency count
+text.freq.list<-table(wordVector.txt)
+#text.freq.list
+library(dplyr)
+
+#converting into a DF
+textDF<-tbl_df(text.freq.list)
+#head(textDF,30)
+#View(textDF)
+#textDF$n
+#textDF$wordVector.txt
+colnames(textDF)<-c("Word","Frequency")
+head(textDF)
+#selecting a random variable
+sample_n(textDF,1)
+
+#getting valid itemsets
+textMoreThan10Freq<-filter(textDF,textDF$Frequency>freq)
+#textMoreThan10Freq<-gsub('\\a+', '', textMoreThan10Freq)
+#textMoreThan10Freq<-gsub('\\an+', '', textMoreThan10Freq)
+#textMoreThan10Freq<-gsub('\\the+', '', textMoreThan10Freq)
+#textMoreThan10Freq<-gsub('\\was+', '', textMoreThan10Freq)
+head(textMoreThan10Freq)
+#text.sortedFreq.list <- sort(text.freq.list)
+#head(text.sortedFreq.list)
+#text.sortedFreq.list
+#text.sortedTable<-paste(names(text.sortedFreq.list),text.sortedFreq.list,sep="    ")
+#head(text.sortedTable)
+
+wordcloud(textMoreThan10Freq$Word, random.order = FALSE, max.words = 1500, col = brewer.pal(9,"Blues"),random.color = TRUE)
